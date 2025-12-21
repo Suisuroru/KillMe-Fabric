3aa4eaeaaaceb6c7079a5f4e4d7d9cf3cd5d1ad7
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-12-22 00:46:24 +0800

    function module

--- src/main/java/fun/bm/killme/Killme.java
+++ src/main/java/fun/bm/killme/Killme.java
@@ -1,10 +1,35 @@
 package fun.bm.killme;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import com.mojang.brigadier.context.CommandContext;
 
 public class Killme implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("killme")
+                .executes(this::executeKill)
+            );
+        });
+    }
+
+    private int executeKill(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+
+        if (source.getPlayer() == null) {
+            source.sendFeedback(() -> Text.literal("该命令只能由玩家执行"), false);
+            return 0;
+        }
+
+        ServerPlayerEntity player = source.getPlayer();
+        player.kill(player.getEntityWorld());
+        source.sendFeedback(() -> Text.literal("已自杀"), true);
+        return 1;
     }
 }
